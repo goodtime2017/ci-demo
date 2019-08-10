89c6f1a1efd36819137fd3ac569ecb70ffbbab14
goodtime2017 <lostdays@126.com>
2019-08-10 16:47:20 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include "circle.h"
+#include "square.h"
+
+using namespace std;
+
+int main() {
+    Circle c;
+    Square s;
+    cout << PI << endl;
+    return 0;
+}
